84cd740f8aeffb22b817af848c5a9dcda3b5e7a5
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-10 22:20:39 -0300

    main

---
+++ app.js
@@ -0,0 +1,33 @@
+let leno = 19;
+if (leno >= 18) {
+  console.log("maior que 18");
+} else {
+  console.log("menor que 18");
+}
+
+let idade = 18;
+let humano = true;
+if (idade >= 18 && humano == true) {
+  console.log("Humano e maior de Idade");
+} else {
+  console.log("não é humano e menor");
+}
+
+let aniversario = "janeiro";
+if (aniversario == "janeiro") {
+  console.log("faz aniversario em janeiro");
+} else {
+  console.log("não faz aniversario");
+}
+
+let nome = "Rafael";
+if (nome[0] === "R") {
+  console.log("Seu nome começa com a letra R");
+} else {
+  console.log("Seu nome não começa com a letra R");
+}
+
+let sobrenome = 'silva'
+if(sobrenome.length > 6 || nome [0]=='E' ) {
+    console.log('Seu sobrenome tem mais de 6 letras e começa com E')
+}else{console.log('Seu sobrenome não tem mais de 6 letras e não começa com a letra E ')}
